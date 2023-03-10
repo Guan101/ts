8f35591fb11d704d2c8fece5e9ad5663416dfacc
guanwenhao <407818306@qq.com>
2023-03-10 15:24:20 +0800

    update at 2023-03-10 15:24:20

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ time=$(date "+%Y-%m-%d %H:%M:%S")
 
 git add .
 git commit -m "update at $time"
-git push -f git@github.com:Guan101/ts.git main
+git push -f git@github.com:Guan101/ts.git note
 # 打包生成静态文件
 npm run build
 
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 
 # 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
 # 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
-git push -f git@github.com:Guan101/Guan101.github.io.git main
+git push -f git@github.com:Guan101/ts.git page
 # gitbash 下 运行 npm run deploy 既可一键部署
 cd -
